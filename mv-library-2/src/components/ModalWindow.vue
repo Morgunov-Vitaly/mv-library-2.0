07d7bb7a0b8e1156809e926d3519655037c51c40
Morgunov-Vitaly <morgunov.vitaly@gmail.com>
2020-02-10 23:42:37 +0300

    Поигрался дома с модальным окном

---
+++ mv-library-2/src/components/ModalWindow.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="overlay" v-show="isOpen">
+    <div class="modal_window">
+      <div class="close_button" @click="toClose">&times;</div>
+      <h2>Title</h2>
+      <p>{{isOpen}}</p>
+      <slot class="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isOpen: {
+      type: Boolean
+    }
+  },  
+  methods: {
+    toClose() {      
+      console.log(this.Open);
+      this.$emit("toClose", false);
+    }
+  }
+};
+</script>
+<style>
+.overlay {
+  display: block;
+  position: fixed;
+  top: 0;
+  /* overflow: hidden; */
+  margin: 0;
+  padding: 0;
+  border: none;
+  width: 100vw;
+  height: 100vh;
+  background-color: #383940f2;
+  z-index: 1100;
+}
+.modal_window {
+  display: block;
+  width: 75vw;
+  max-width: 1200px;
+  min-height: 80vh;
+  margin: auto;
+  margin-top: 7vh;
+  position: relative;
+  padding: 30px;
+  background-color: white;
+  color: #1e1e1e;
+  border: none;
+  border-radius: 3px;
+}
+.close_button {
+  color: #ffffff;
+  background-color: #c3c3c3;
+  /* border: 1px solid #1e1e1e; */
+  border-radius: 50%;
+  position: absolute;
+  top: 10px;
+  line-height: 35px;
+  font-size: 35px;
+  right: 10px;
+  height: 40px;
+  width: 40px;
+  cursor: pointer;
+}
+</style>
--- mv-library-2/src/views/Home.vue
+++ mv-library-2/src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <modalWindow v-bind:isOpen="isOpen" @toClose="toClose">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi dolorum officiis ipsum odio minima itaque officia placeat dolorem. Fugiat maxime cumque odit nisi sunt possimus mollitia eaque eius expedita eligendi. 
+    </modalWindow>
+    <button
+    @click="toOpen">Открыть окно</button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -8,11 +13,28 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import modalWindow from '@/components/ModalWindow.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    modalWindow
+  },
+  data(){
+    return{
+      isOpen: false
+    }
+  },
+  methods:{
+    toOpen(){
+      this.isOpen = true;
+      console.log(this.isOpen);
+    },
+    toClose(isOpen){
+      this.isOpen = isOpen;
+      console.log(this.isOpen);
+    }
   }
 }
 </script>
